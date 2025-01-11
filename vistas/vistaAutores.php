4037a89f2c62876b3a493799fb4798b9b2109587
nicolarus05 <ngh2605@gmail.com>
2025-01-11 20:38:28 +0100

    cambios realizados en vistaAutores

--- vistas/vistaAutores.php
+++ vistas/vistaAutores.php
@@ -1,31 +1,30 @@
 <!-- Cuerpo de la vista de autores -->
+<main>
+    <h2>Listado de Autores</h2>
 
-<h1>Listado de Autores</h1>
+    <table>
+        <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Nacionalidad</th>
+        </tr>
 
-<table>
-    <tr>
-        <th>Nombre</th>
-        <th>Apellido</th>
-        <th>Nacionalidad</th>
-        <th>Fecha de Nacimiento</th>
-    </tr>
+        <?php 
+            $autores = $datos;
 
-    <?php 
-        $autores = $datos['autores'];
+            foreach($autores as $autor) {
+                echo "<tr>";
+                    echo "<td>$autor[Nombre]</td>";
+                    echo "<td>$autor[Apellidos]</td>";
+                    echo "<td>$autor[Pais]</td>";
 
-        foreach($autores as $autor) {
-            echo "<tr>";
-                echo "<td>$autor[nombre]</td>";
-                echo "<td>$autor[apellido]</td>";
-                echo "<td>$autor[nacionalidad]</td>";
-                echo "<td>$autor[fechaNacimiento]</td>";
+                    if(isset($_SESSION['usuario']) && ($_SESSION['rol']!="registrado")){
+                        echo "<td><a href='actualizarAutor.php?id=".$autor['id']."'>Actualizar</a></td>";
+                        echo "<td><a href='borrarAutor.php?id=".$autor['id']."'>Borrar</a></td>";
+                    }
 
-                if(isset($_SESSION['logged']) && $_SESSION['logged']!="registrado"){
-                    echo "<td><a href='actualizarAutor.php?id=".$autor['id']."'>Actualizar</a></td>";
-                    echo "<td><a href='borrarAutor.php?id=".$autor['id']."'>Borrar</a></td>";
-                }
-
-            echo "</tr>";
-        }
-    ?>
-</table>
+                echo "</tr>";
+            }
+        ?>
+    </table>
+</main>
