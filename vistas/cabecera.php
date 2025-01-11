54160e132a3c9f2a9458e8d6b5cc9a8efa7ba494
SOrtMur <salvadorom1996@gmail.com>
2025-01-11 21:13:48 +0100

    vistaGeneral y cabecera checked

--- vistas/cabecera.php
+++ vistas/cabecera.php
@@ -8,4 +8,30 @@
 <body>
     <header>
         <h1>Bienvenido a la Biblioteca</h1>
+        <nav id='menu'>
+            <ul>
+            <?php 
+                if(isset($_SESSION['rol'])){
+                    if($_SESSION['rol']=='bibliotecario'){
+                        echo "<li><a href='?vista=vistaLibros'>Listado de libros</a></li>";
+                        echo "<li><a href='?vista=vistaAutores'>Listado de autores</a></li>";
+                        echo "<li><a href='?vista=insertarLibro'>Insertar libro</a></li>";
+                    } elseif ($_SESSION['rol']=='admin'){
+                        echo "<li><a href='?vista=vistaUsuarios'>Listado de usuarios</a></li>";
+                        echo "<li><a href='?vista=insertarUsuario'>Insertar usuario</a></li>";
+                        echo "<li><a href='?vista=vistaLibros'>Listado de libros</a></li>";
+                        echo "<li><a href='?vista=vistaAutores'>Listado de autores</a></li>";
+                    } elseif ($_SESSION['rol']=='registrado'){
+                        echo "<li><a href='?vista=vistaLibros'>Listado de libros</a></li>";
+                        echo "<li><a href='?vista=vistaAutores'>Listado de autores</a></li>";
+                    }
+                    echo "<li><a href='?vista=miPerfil'>Mi Perfil</a></li>"; 
+                    echo "<li><a href='?vista=cerrarSesion'>Cerrar sesion</a></li>";
+                    echo "</ul>"; 
+                } else {
+                    echo "<h2>Log In</h2>";
+                }
+            ?>
+        </nav>
+
     </header>
---
+++ vistas/vistaGeneral.php
@@ -0,0 +1,3 @@
+<main>
+    <h2>Registrado como <?php echo $datos;?></h2>
+</main>
