36a35713c27f6b48631bbdcc228643faf7f1621b
SOrtMur <salvadorom1996@gmail.com>
2025-01-11 11:25:45 +0100

    index con vistaLogin y vistaGeneral

--- index.php
+++ index.php
@@ -1,18 +1,50 @@
 <?php
+session_start();
 
-include './modelos/usuarios.php';
-include './vistas/vista.php';
-include './modelos/modelo.php';
+if(!file_exists("seguridad/config.php")){
+    header("Location:./seguridad/install.php");
+}
 
-//lógica para determinar qué mostrar
-$accion = $_GET['accion'] ?? 'usuarios';
+require_once 'seguridad/conexion.php';
+require_once 'seguridad/seguridad.php';
+$segura = new Seguridad(new Conexion());
+
+require_once './modelos/usuarios.php';
+require_once './vistas/vista.php';
+require_once './modelos/autores.php';
+require_once './modelos/libros.php';
+
+if(isset($_POST['login'])){
+    $usuario = $_POST['usuario'];
+    $password = $_POST['password'];
+
+    if($segura->login($usuario,$password)){
+        $_GET['vista'] = 'vistaGeneral';
+    } else {
+        $_GET['vista'] = 'vistaLogin';
+    }
+}
+
+if(isset($_GET['vista'])){
+    $vista = $_GET['vista'];
+}
+
+if(!isset($vista)){
+    $vista = null;
+}
 
 switch ($accion) {
     case 'usuarios':
         //qué hacer para mostrar los usuarios
         break;
     default:
-        echo "Acción no válida";
+        if($segura->isLogged()){
+            $datos = $segura->getRol(); 
+            Vista::mostrar('vistaGeneral',$datos);
+        } else {
+            Vista::mostrar('vistaLogin');
+        }
+        break;
 }
 
 ?>
